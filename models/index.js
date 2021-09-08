9896523ea5041672fa9265ee02cbbe25e16c316d
tassoni13 <tassoni13@gmail.com>
2021-09-08 14:59:37 -0400

    Added model associations to index

---
+++ models/index.js
@@ -0,0 +1,34 @@
+const User = require('./user');
+const Post = require('./post');
+const Comment = require('./comment');
+
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelte: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Comment };
+
+
