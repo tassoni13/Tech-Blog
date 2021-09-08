38694cf1ac0554a9adf9cf5a58172db9eebcd23a
tassoni13 <tassoni13@gmail.com>
2021-09-08 13:59:37 -0400

    Added user models

--- models/user.js
+++ models/user.js
@@ -1,1 +1,46 @@
-//use models
+const {Model, DataTypes} = require('sequelize');
+const bycrypt = require('bycrypt');
+const sequelize = require('../config/connection');
+
+class User extends Model {
+    checkPassword(loginPw) {
+        return bycrypt.compareSync(loginPw, this.password);
+    }
+}
+
+User.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [8],
+        },
+    },
+},
+{hooks: {
+    beforeCreate: async (newUserData) => {
+        newUserData.password = await bycrypt.hash(newUserData.password, 10);
+        return newUserData;
+    },
+    beforeUpdate: async (updatedUserData) => {
+        return updatedUserData;
+    },
+},
+sequelize,
+timestamps: false,
+freezeTableName: true,
+underscored: true,
+modelName: 'user',
+});
+
+module.exports = User;
